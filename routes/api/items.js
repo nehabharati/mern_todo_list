ed190a23381ffbdd00b591f3802954e48ec6e052
Neha Bharati <nehabharati@Nehas-MacBook-Pro.local>
2020-09-26 11:36:11 +0530

    MERN todo list with authentication

---
+++ routes/api/items.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+
+//Item model
+const Item = require("../../models/Item");
+
+//@route GET api/items
+//@desc Get all items
+//@access Public
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ name: 1 }) // -1 means descending order
+    .then((items) => res.json(items));
+});
+
+//@route POST api/items
+//@desc Create an item
+//@access Private
+router.post("/", auth, (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem.save().then((item) => res.json(item));
+});
+
+//@route Delete api/items/:id
+//@desc Delete an item
+//@access Private
+router.delete("/:id", auth, (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) => item.remove())
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+
+const items = require("./routes/api/items");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+
+const app = express();
+
+//BodyParser Middleware
+app.use(express.json());
+
+//DB config
+const db = config.get("mongoURI");
+
+//Connect to mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+//Use routes
+app.use("/api/items", items);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => console.log(`Server is listening on ${port}`));
